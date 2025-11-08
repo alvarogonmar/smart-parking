87c74a0256b3159e372d96f89e8a8990c9d41f6f
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-08 12:50:07 -0600

    Fix firebaseConfig.js to include apiKey in the Firebase configuration

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,3 +4,4 @@ import { getFirestore } from "firebase/firestore";
 
 // Configuración de Firebase usando variables de entorno de Vite
 const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
