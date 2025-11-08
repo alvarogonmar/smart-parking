e4a196e38e9524556298746fbdb75128fb449da6
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-08 12:49:59 -0600

    Add firebase configuration setup to firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,3 +1,6 @@
 // Importa las funciones necesarias del SDK de Firebase
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
+// Configuración de Firebase usando variables de entorno de Vite
+const firebaseConfig = {
