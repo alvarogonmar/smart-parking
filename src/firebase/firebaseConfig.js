40286b2bb56b1fcdd0bb11d4f0e5834031dd79f2
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-08 12:50:28 -0600

    Fix firebaseConfig.js to include storageBucket in the Firebase configuration

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,3 +7,4 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
