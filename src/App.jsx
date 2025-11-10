7ab3c706d4f5b9360e2f29c4c7670c9c54cf0ea1
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-10 07:06:18 -0600

    Import Firestore database and ParkingSlot component in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,4 @@
 import React, { useEffect, useState } from "react";
 import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "./firebase/firebaseConfig";
+import ParkingSlot from "./components/ParkingSlot";
