6c46f0345cff47cae38df4c2a07b4230f15e5521
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-11 06:57:39 -0600

    Refactor App component imports for better organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "./firebase/firebaseConfig";
 import ParkingSlot from "./components/ParkingSlot";
-import "./App.css";
-import "./components/ParkingSlot.css";
-
+import Header from "./components/Header";
+import LogInPage from "./pages/LogInPage";
+import Dashboard from "./pages/Dashboard";
+import "../src/css/App.css"
+import "../src/css/ParkingSlot.css";
 function App() {
   const [spots, setSpots] = useState([]);
 
@@ -12,3 +15,6 @@ function App() {
     const spotsCollection = collection(db, "parking-slots");
 
     const unsubscribe = onSnapshot(spotsCollection, (snapshot) => {
+      const spotsData = snapshot.docs.map((doc) => ({
+        id: doc.id,
+
