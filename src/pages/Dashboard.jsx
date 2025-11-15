1df37472807f409a6383f283d8cd5d2e68d258b9
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-15 10:36:50 -0600

    Add missing import statements for UsageByZoneChart, UsageFrequencyChart, and UsageCurrentSection in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,3 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
 import UsageBySlotChart from "../components/dashboard/UsageBySlotChart";
+import UsageByZoneChart from "../components/dashboard/UsageByZoneChart";
+import UsageFrequencyChart from "../components/dashboard/UsageFrequency";
+import UsageCurrentSection from "../components/dashboard/UsageCurrentSection";
