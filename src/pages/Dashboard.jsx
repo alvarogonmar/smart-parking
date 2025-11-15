e29ee7730c6e3f25fc996e340831fbb0d97ff324
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-15 10:36:37 -0600

    Add import statements for Firebase configuration and UsageBySlotChart in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase/firebaseConfig";
+import UsageBySlotChart from "../components/dashboard/UsageBySlotChart";
