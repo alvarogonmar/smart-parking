8b2a87cefa6d703fea2d24806e77f4d8387676de
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-28 13:40:35 -0600

    Add styling for no data message in UsageByZoneChart component

--- src/components/dashboard/UsageByZoneChart.jsx
+++ src/components/dashboard/UsageByZoneChart.jsx
@@ -66,3 +66,13 @@ function UsageByZoneChart({ data }) {
         <div
           style={{
             position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+            pointerEvents: "none",
+            color: "#9ca3af",
+            fontWeight: 600,
+            fontSize: 16,
+            textAlign: "center",
+          }}
+        >
