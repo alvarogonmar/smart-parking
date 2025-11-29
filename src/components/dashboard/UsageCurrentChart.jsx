276f8bbffcf7ed304cfcc50d1e8de503f2cde649
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-29 14:08:35 -0600

    Add Pie component to UsageCurrentChart for rendering chart data

--- src/components/dashboard/UsageCurrentChart.jsx
+++ src/components/dashboard/UsageCurrentChart.jsx
@@ -16,3 +16,13 @@ function UsageCurrentChart({ data }) {
 
   return (
     <PieChart width={300} height={300}>
+      <Pie
+        data={chartData}
+        dataKey="value"
+        nameKey="name"
+        cx="50%"
+        cy="50%"
+        outerRadius={100}
+        fill="#8884d8"
+        label
+      >
