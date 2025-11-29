e6c60c82351e027adfe2e9251f961f368aaf68f1
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-29 14:07:21 -0600

    Add no data message to UsageCurrentChart component

--- src/components/dashboard/UsageCurrentChart.jsx
+++ src/components/dashboard/UsageCurrentChart.jsx
@@ -2,3 +2,4 @@ import React from "react";
 import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 
 function UsageCurrentChart({ data }) {
+  if (!data || data.length === 0) return <p>No hay datos disponibles</p>;
