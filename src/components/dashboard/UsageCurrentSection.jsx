5741c8426e1ca448bc7de7da8de9e5db5067736f
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-30 13:18:33 -0600

    Add occupancy calculation in UsageCurrentSection component

--- src/components/dashboard/UsageCurrentSection.jsx
+++ src/components/dashboard/UsageCurrentSection.jsx
@@ -4,3 +4,5 @@ import UsageCurrentChart from "./UsageCurrentChart";
 function UsageCurrentSection({ slots }) {
   const occupiedCount = slots.filter(s => s.isOccupied).length;
   const freeCount = slots.filter(s => !s.isOccupied).length;
+  const occupancy =
+    slots.length > 0 ? ((occupiedCount / slots.length) * 100).toFixed(1) : 0;
