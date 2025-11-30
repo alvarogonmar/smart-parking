dc3f041571ef93914db038b8c2d7093ce53c2891
alvarogonmar <alvarogonmar0@gmail.com>
2025-11-30 13:18:16 -0600

    Add occupiedCount calculation in UsageCurrentSection component

--- src/components/dashboard/UsageCurrentSection.jsx
+++ src/components/dashboard/UsageCurrentSection.jsx
@@ -2,3 +2,4 @@ import React from "react";
 import UsageCurrentChart from "./UsageCurrentChart";
 
 function UsageCurrentSection({ slots }) {
+  const occupiedCount = slots.filter(s => s.isOccupied).length;
