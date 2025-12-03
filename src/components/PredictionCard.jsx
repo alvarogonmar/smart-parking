ff5883e1ec111a4ce02a9ff05338753e64162841
alvarogonmar <alvarogonmar0@gmail.com>
2025-12-03 07:16:33 -0600

    Add useEffect hook to PredictionCard component

--- src/components/PredictionCard.jsx
+++ src/components/PredictionCard.jsx
@@ -4,3 +4,5 @@ import { trainParkingModel, predictSlot } from "../ai/trainModel";
 function PredictionCard({ slots }) {
   const [model, setModel] = useState(null);
   const [prediction, setPrediction] = useState(null);
+
+  useEffect(() => {
