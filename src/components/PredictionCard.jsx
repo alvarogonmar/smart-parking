46e213c8a8cb0dd003cb5e5dd504510aa087ab79
alvarogonmar <alvarogonmar0@gmail.com>
2025-12-03 07:16:11 -0600

    Add state management for model in PredictionCard component

--- src/components/PredictionCard.jsx
+++ src/components/PredictionCard.jsx
@@ -2,3 +2,4 @@ import React, { useEffect, useState } from "react";
 import { trainParkingModel, predictSlot } from "../ai/trainModel";
 
 function PredictionCard({ slots }) {
+  const [model, setModel] = useState(null);
